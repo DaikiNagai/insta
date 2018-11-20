7098ae7ce7bef814b5580ae1dcd4f0949d8d2ab5
永井大基 <01prankster@gmail.com>
2018-11-20 10:59:51 +0900

    mailConfirm

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
+  add_flash_types :success
+
+    protect_from_forgery with: :exception
+
+    before_action :configure_permitted_parameters, if: :devise_controller?
 
   PERMISSIBLE_ATTRIBUTES = %i(avatar avatar_cache content content_cache)
+  def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: PERMISSIBLE_ATTRIBUTES)
+      devise_parameter_sanitizer.permit(:account_update, keys: PERMISSIBLE_ATTRIBUTES)
+  end
 end
